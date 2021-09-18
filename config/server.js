53caa437cc0c6575345a9e6b46fb85f0041cb189
Igor Veludo <velvet.guimaraes@gmail.com>
2021-09-18 18:26:33 -0300

    Ajustando configurações do banco

--- config/server.js
+++ config/server.js
@@ -1,9 +1,11 @@
+require('dotenv/config')
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: process.env.HOST,//env('HOST', '0.0.0.0'),
+  port: process.env.PORT,//env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '0e27e090442b2cd90116cbdbd5b80b1f'),
+      secret: process.env.ADMIN_JWT_SECRET//env('ADMIN_JWT_SECRET', '0e27e090442b2cd90116cbdbd5b80b1f'),
     },
   },
 });
