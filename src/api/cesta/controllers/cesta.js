e6abb485a25a8c84aac02df8281e562855ccfd27
Felipe Agustini <felipeboccardi@hotmail.com>
2023-03-15 20:14:26 -0300

    #36 Gera cestas

---
+++ src/api/cesta/controllers/cesta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cesta controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cesta.cesta');
---
+++ src/api/cesta/services/cesta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cesta service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::cesta.cesta');
