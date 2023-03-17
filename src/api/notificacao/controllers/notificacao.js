d4d35b99ef1638eab269f8f991a430749455f72e
Felipe Agustini <felipeboccardi@hotmail.com>
2023-03-17 08:29:35 -0300

    #41 Gera notificações

---
+++ src/api/notificacao/controllers/notificacao.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * notificacao controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::notificacao.notificacao');
---
+++ src/api/notificacao/routes/notificacao.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * notificacao router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::notificacao.notificacao');
