2c3fcf20ca6f2969f8dfcda43d9507cd3e184eef
giovannabbottino <giovannabbottino@gmail.com>
2023-03-21 18:40:06 -0300

    #43 gera extrato

---
+++ src/api/extrato/controllers/extrato.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * extrato controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::extrato.extrato');
