afff6b12a1f5e0dcbc638191e80092beb463e118
giovannabbottino <giovannabbottino@gmail.com>
2023-04-10 19:56:03 -0300

    #41 remove notificacoes

--- src/api/notificacao/controllers/notificacao.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * notificacao controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::notificacao.notificacao');
