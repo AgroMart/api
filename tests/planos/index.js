749a774b0372ae53c438b40c3a38678c8d91878c
Felipe Agustini <felipeboccardi@hotmail.com>
2023-05-06 13:18:36 -0300

    #60 Adiciona teste de modificação de planos
    Co-authored-by: giovannabbottino <giovannabbottino@gmail.com>

---
+++ tests/planos/index.js
@@ -0,0 +1,80 @@
+// api.put(`planos/${item.id}`, body);
+// const body = {
+//           quantidade: item.stock - item.quantity,
+//         };
+
+const request = require('supertest');
+
+describe('Testes de planos', () => {
+    const path = "/cestas";
+    const mockUserData = {
+        username: "userregister",
+        email: "userregister@strapi.com",
+        password: "1234userregister"
+      };
+    beforeAll(async () => {
+        response = await request(strapi.server.httpServer)
+            .post('/auth/local')
+            .send({
+                identifier: mockUserData.email,
+                password: mockUserData.password});
+        user = response.body.user
+        jwt = response.body.jwt
+
+        await request(strapi.server.httpServer)
+        .post('/lojas')
+        .set("Authorization",`Bearer  ${jwt}`)
+        .send({
+            nome: 'lojaLoja',
+            descricao: 'loja para o teste de produtos',
+            endereco: 1,
+        });
+
+        loja = response.body
+        subscriberBody = {
+            nome: user.username,
+            cestas_disponiveis: 0,
+            planos: plano.id,
+            usuario: user.id,
+            loja: loja.id
+        };
+
+        response = await request(strapi.server.httpServer)
+            .post( "/assinantes") 
+            .send({
+                nome: user.username,
+                cestas_disponiveis: 0,
+                usuario: user.id,
+                loja: loja.id
+            });
+        assinantes = response.body
+
+        response = await request(strapi.server.httpServer)
+        .post(path).send({
+            nome: "Teste de planos",
+            descricao: "planos",
+            valor: 1.59,
+            quantidade: 2,
+            quantidade_de_cestas: 1,
+            loja: loja.id,
+            assinantes: [assinantes.id]
+        }).set("Authorization",`Bearer  ${jwt}`);
+        plano = response.body
+    });
+
+    
+    it("Altera quantidade de planos", async () => {
+        await request(strapi.server.httpServer)
+            .put(`${path}/${plano.id}`).send({
+                quantidade: plano.quantidade -1,
+            })
+            .set("accept", "application/json")
+            .set("Authorization",`Bearer  ${jwt}`)
+            .set("Content-Type", "application/json")
+            .expect(200)
+            .then((data) => {
+                expect(data.body).toBeDefined();
+                expect(data.body.quantidade).toBe(1);
+            });
+    });
+});
