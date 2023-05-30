9f42ae9c73a92dbe3d24b0f111ab5f3482c73044
giovannabbottino <giovannabbottino@gmail.com>
2023-05-30 20:17:52 -0300

    #70 adiciona novo plugin

--- config/plugins.js
+++ config/plugins.js
@@ -2,4 +2,8 @@ module.exports = ({ env }) => ({
     "expo-notifications": {
       enabled: true,
     },
+    'pagamento': {
+      enabled: true,
+      resolve: './src/plugins/pagamento'
+    },
   });
