bdb4af67777102734504b1d3621132795f3b450b
Felipe Agustini <felipeboccardi@hotmail.com>
2023-07-16 17:10:54 -0300

    Altera load

--- src/plugins/pagamento/admin/src/components/GatewayCustom/index.js
+++ src/plugins/pagamento/admin/src/components/GatewayCustom/index.js
@@ -126,18 +126,26 @@ const GatewayCustom = ({ gateway }) => {
     const saveReqData = loadReqDataFields();
     const savedParams = loadParamsFields();
     if (savedParams) {
-      const paramsArray = Object.entries(savedParams).map(([key, value]) => ({
-        variable: key,
-        systemVariable: JSON.stringify(value),
-      }));
-      handleLoadParamField(paramsArray);
+      try {
+        const paramsArray = Object.entries(savedParams).map(([key, value]) => ({
+          variable: key,
+          systemVariable: JSON.stringify(value),
+        }));
+        handleLoadParamField(paramsArray);
+      } catch (error) {
+        handleLoadParamField(savedParams);
+      }
     }
     if (saveReqData) {
-      const reqArray = Object.entries(saveReqData).map(([key, value]) => ({
-        variable: key,
-        systemVariable: JSON.stringify(value),
-      }));
-      handleLoadReqField(reqArray);
+      try {
+        const reqArray = Object.entries(saveReqData).map(([key, value]) => ({
+          variable: key,
+          systemVariable: JSON.stringify(value),
+        }));
+        handleLoadReqField(reqArray);
+      } catch (error) {
+        handleLoadReqField(saveReqData);
+      }
     }
   }, []);
 
